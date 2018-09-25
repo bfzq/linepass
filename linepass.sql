f2563714d637e1545d2934ff43a410aeb9bd6052
bfzq <280372646@qq.com>
2018-09-25 04:56:01 -0400

    修改表结构，修改表之间的约束关系喝字符集

--- linepass.sql
+++ linepass.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.21, for macos10.13 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: linepass
+-- Host: localhost    Database: linepass
 -- ------------------------------------------------------
--- Server version	5.7.21
+-- Server version	5.7.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,6 +24,62 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `linepass` /*!40100 DEFAULT CHARACTER S
 USE `linepass`;
 
 --
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(11) unsigned NOT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  `title` varchar(16) DEFAULT NULL COMMENT '记录标题',
+  `account` varchar(32) NOT NULL COMMENT '账户',
+  `passwd` varchar(64) DEFAULT NULL COMMENT '账户密码',
+  `nickname` varchar(32) DEFAULT NULL COMMENT '账户昵称',
+  PRIMARY KEY (`id`),
+  KEY `FK_accounts_users` (`user_id`),
+  KEY `FK_accounts_company` (`company_id`),
+  CONSTRAINT `FK_accounts_company` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `FK_accounts_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='个人账号';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ps_name` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Index_name` (`ps_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='公司名称';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,8 +91,9 @@ CREATE TABLE `users` (
   `username` varchar(256) NOT NULL DEFAULT '',
   `password` varchar(256) NOT NULL DEFAULT '',
   `nickname` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='linepass login account';
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Index_usrename` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='登陆用户';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +102,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','7B2FFF48F9397BF933E32EA46030DBE4','admin_nk');
+INSERT INTO `users` VALUES (1,'admin','7B2FFF48F9397BF933E32EA46030DBE4','admin_test');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-23 11:05:48
+-- Dump completed on 2018-09-25  4:47:42
